4616fc5b2065e66d7904835bc0cc0d2166e13087
elmiramm <elmira.m.m@yandex.ru>
2023-06-16 09:44:01 +0300

    js - checkboxes, sorting, style for checkered checkboxes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
 const tasks = [
-	'Running',
-	'Programming',
-	'REceive a parsle',
-	'Meeting with friend',
-	'Learning englisg/korean',
-	'Prepare for the test',
+	{ text: 'Running', done: true },
+	{ text: 'Programming', done: false },
+	{ text: 'REceive a parsle', done: false },
+	{ text: 'Meeting with friend', done: false },
+	{ text: 'Learning englisg/korean', done: false },
+	{ text: 'Prepare for the test', done: false },
 ];
 
 const renderListItems = listItems => {
 	const listElem = document.querySelector('.list');
 
-	const listItemsElems = listItems.map(itemText => {
-		const listItemElem = document.createElement('li');
-		listItemElem.classList.add('list__item');
-		const checkboxElem = document.createElement('input');
-		checkboxElem.classList.add('list__item-checkbox');
-		checkboxElem.setAttribute('type', 'checkbox');
+	const listItemsElems = listItems
+		.sort((a, b) => a.done - b.done)
+		.map(({ text, done }) => {
 
-		listItemElem.append(checkboxElem, itemText);
-		return listItemElem;
-	});
+			const listItemElem = document.createElement('li');
+			listItemElem.classList.add('list__item');
+			if (done) {
+				listItemElem.classList.add('list__item_done');
+			}
+
+			const checkboxElem = document.createElement('input');
+			checkboxElem.classList.add('list__item-checkbox');
+			checkboxElem.setAttribute('type', 'checkbox');
+			checkboxElem.checked = done;
+
+			listItemElem.append(checkboxElem, text);
+
+			return listItemElem;
+		});
 
 	listElem.append(...listItemsElems);
 }
